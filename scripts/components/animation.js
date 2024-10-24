19e39ecee96f86dab534ab46e529399d5d53c3f3
Fedja <Fedja@fedja.com>
2024-10-24 12:24:56 +0300

    Updates

--- scripts/components/animation.js
+++ scripts/components/animation.js
@@ -1,4 +1,5 @@
 const textItems = document.querySelectorAll('.item-text');
+const btnItems = document.querySelectorAll('.btn--clip');
 
 if (textItems.length > 0) {
   const intersectionCallback = (entries) => {
@@ -15,5 +16,23 @@ if (textItems.length > 0) {
 
   window.addEventListener('load', (event) => {
     textItems.forEach((item) => observer.observe(item))
+  }, false);
+}
+
+if (btnItems.length > 0) {
+  const intersectionCallback = (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('is-show');
+      }
+    });
+  };
+
+  const observer = new IntersectionObserver(intersectionCallback, {
+    threshold: 1,
+  });
+
+  window.addEventListener('load', (event) => {
+    btnItems.forEach((item) => observer.observe(item))
   }, false);
 }
